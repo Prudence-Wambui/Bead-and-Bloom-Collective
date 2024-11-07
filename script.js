ed8369aae831b159c9c28426710a1e094dd7956b
Prudence-Wambui <prudencewambui.official@gmail.com>
2024-11-07 19:46:42 +0300

    saved

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
 
 menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
 
     /*When the icon is clicked on, it will add the sidebar class (small-sidebar), and 
     if the class is already there it removes it */
